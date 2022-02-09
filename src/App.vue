b6246f3102b657b4e4283d09ec0423b79840f433
Digo1979 <programador.1979@gmail.com>
2022-02-09 15:55:13 -0300

    Materio
    Materio

--- src/App.vue
+++ src/App.vue
@@ -1,109 +1,61 @@
 <template>
-  <div id="app">  
-    <Nav />
-    <div class="auth-wrapper">
-      <div class="auth-inner">
-        <router-view />
-      </div>
-    </div>
-
-  </div>
+  <component
+    :is="resolveLayoutVariant"
+    :class="`skin-variant--${appSkinVariant}`"
+  >
+    <transition
+      :name="appRouteTransition"
+      mode="out-in"
+      appear
+    >
+      <router-view></router-view>
+    </transition>
+  </component>
 </template>
 
 <script>
-import Nav from './components/Nav.vue'
-import axios from 'axios'
+// eslint-disable-next-line object-curly-newline
+import { computed } from '@vue/composition-api'
+// eslint-disable-next-line import/no-unresolved
+import useAppConfig from '@core/@app-config/useAppConfig'
+import { useRouter } from '@core/utils'
+import { useLayout } from '@core/layouts/composable/useLayout'
+
+// Layouts
+import LayoutContentVerticalNav from '@/layouts/variants/content/vertical-nav/LayoutContentVerticalNav.vue'
+import LayoutContentHorizontalNav from '@/layouts/variants/content/horizontal-nav/LayoutContentHorizontalNav.vue'
+import LayoutBlank from '@/layouts/variants/blank/LayoutBlank.vue'
+
+// Dynamic vh
+import useDynamicVh from '@core/utils/useDynamicVh'
 
 export default {
-  name: 'App',
   components: {
-    Nav,
+    LayoutContentVerticalNav,
+    LayoutContentHorizontalNav,
+    LayoutBlank,
   },
-  async created() {
-      const response = await axios.get('user');
-      this.$store.dispatch('user',response.data);
-  }
+  setup() {
+    const { route } = useRouter()
+    const { appContentLayoutNav, appSkinVariant, appRouteTransition } = useAppConfig()
+
+    const { handleBreakpointLayoutSwitch } = useLayout()
+    handleBreakpointLayoutSwitch()
+
+    const resolveLayoutVariant = computed(() => {
+      if (route.value.meta.layout === 'blank') return 'layout-blank'
+      if (route.value.meta.layout === 'content') return `layout-content-${appContentLayoutNav.value}-nav`
+
+      return null
+    })
+
+    useDynamicVh()
+
+    return {
+      resolveLayoutVariant,
+      appSkinVariant,
+      appRouteTransition,
+    }
+  },
 }
 </script>
-
-<style>
-  @import url('https://fonts.googleapis.com/css?family=Fira+Sans:400,500,600,700,800');
-
-  * {
-    box-sizing: border-box;
-  }
-
-  body {
-    background: #1c8ef9 !important;
-    min-height: 100vh;
-    display: flex;
-    font-weight: 400;
-    font-family: 'Fira Sans', sans-serif;
-  }
-
-  h1, h2, h3, h4, h5, h6, label, span {
-    font-weight: 500;
-    font-family: 'Fira Sans', sans-serif;
-  }
-
-  body, html, #app, #root, .auth-wrapper {
-    width: 100%;
-    height: 100%;
-  }
-
-  #app {
-    text-align: center;
-  }
-
-  .navbar-light {
-    background-color: #ffffff;
-    box-shadow: 0px 14px 80px rgba(34 ,35 ,58 ,0.2);
-  }
-
-  .auth-wrapper {
-    display: flex;
-    justify-content: center;
-    flex-direction: column;
-    text-align: left;
-  }
-
-  .auth-inner {
-    width: 450px;
-    margin: auto;
-    background: #ffffff;
-    box-shadow: 0px 14px 80px rgba(34, 35, 58, 0.2);
-    padding: 40px 55px 45px 55px;
-    border-radius: 15px;
-    transition: all .3s;
-  }
-
-  .auth-wrapper .form-control:focus {
-    border-color: #167bff;
-    box-shadow: none;
-  }
-
-  .auth-wrapper h3 {
-    text-align: center;
-    margin: 0;
-    line-height: 1;
-    padding-bottom: 20px;
-  }
-
-  .custom-control-label {
-    font-weight: 400;
-  }
-
-  .forgot-password, .forgot-password a {
-    text-align: right;
-    font-size: 13px;
-    padding-top: 10px;
-    color: #7f7d7d;
-    margin: 0;
-  }
-
-  .forgot-password a{
-    color: #167bff
-  }  
-
-
-</style>
