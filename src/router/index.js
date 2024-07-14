f6854cc454e92ec8a962dfd8623a2b4169486fbb
Digo1979 <programador.1979@gmail.com>
2024-07-14 18:26:55 -0300

    Rota
    Corrigido problema de layout da rota (login sem fundo do template)

--- src/router/index.js
+++ src/router/index.js
@@ -1,33 +1,68 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Login from '../views/Login.vue'
-import Dashboard from '../views/Dashboard.vue'
-import Calendario from '../views/Calendario.vue'
-import Configuracao from '../views/Configuracao.vue'
-import Privacidade from '../views/Privacidade.vue'
-import TermoUso from '../views/TermoUso.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import AppLayout from '../layouts/AppLayout.vue';
+import Login from "../views/Login.vue";
+import Dashboard from "../views/Dashboard.vue";
+import Calendario from "../views/Calendario.vue";
+import Configuracao from "../views/Configuracao.vue";
+import Privacidade from "../views/Privacidade.vue";
+import TermoUso from "../views/TermoUso.vue";
 
 const routes = [
-  { path: '/', component: Login },
-  { path: '/dashboard', component: Dashboard, meta: { requiresAuth: true } },
-  { path: '/calendario', component: Calendario, meta: { requiresAuth: true } },
-  { path: '/configuracao', component: Configuracao, meta: { requiresAuth: true } },
-  { path: '/privacidade', component: Privacidade, meta: { requiresAuth: false } },
-  { path: '/termouso', component: TermoUso, meta: { requiresAuth: false } }  
-]
+  { 
+    path: '/',
+    component: AppLayout,
+    children: [
+      {
+        path: "",
+        component: Dashboard,
+        meta: { requiresAuth: true },
+      },      
+      {
+        path: "/dashboard",
+        component: Dashboard,
+        meta: { requiresAuth: true },
+      },
+      {
+        path: "/calendario",
+        component: Calendario,
+        meta: { requiresAuth: true },
+      },
+      {
+        path: "/configuracao",
+        component: Configuracao,
+        meta: { requiresAuth: true },
+      },
+      {
+        path: "/privacidade",
+        component: Privacidade,
+        meta: { requiresAuth: false },
+      },
+      {
+        path: "/termouso",
+        component: TermoUso,
+        meta: { requiresAuth: false },
+      },
+    ],
+  },
+  {
+    path: "/login",
+    component: Login,
+  }  
+];
 
 const router = createRouter({
   history: createWebHistory(),
-  routes
-})
+  routes,
+});
 
 router.beforeEach((to, from, next) => {
-  const loggedIn = localStorage.getItem('loggedIn')
+  const loggedIn = localStorage.getItem("loggedIn");
 
-  if (to.matched.some(record => record.meta.requiresAuth) && !loggedIn) {
-    next('/')
+  if (to.matched.some((record) => record.meta.requiresAuth) && !loggedIn) {
+    next("/");
   } else {
-    next()
+    next();
   }
-})
+});
 
-export default router
+export default router;
